82c6ce6ee30dc73a67639cea6182032edf90cf44
tarista13 <tyarista13@gmail.com>
2024-08-24 15:41:15 -0400

    Finished valid parentheses

---
+++ Arrays&Hashing/347-TopKFrequentElements.py
@@ -0,0 +1,24 @@
+"""
+Given an integer array nums an an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+"""
+
+def topKFrequent(nums, k):
+    return True
+
+
+nums1 = [1,1,1,2,2,3]
+k1 = 2
+
+nums2 = [1]
+k2 = 1
+
+print(topKFrequent(nums1, k1))
+print(topKFrequent(nums2, k2))
--- Stack/20-ValidParentheses.py
+++ Stack/20-ValidParentheses.py
@@ -1,3 +1,42 @@
 #create a stack for the opening parentheses
 #create a hashmap for the closing parenetheses that are then linked to a opening parentheses:
 #if the top of stack equals the closing parenthese, pop from top of stack
+
+"""
+You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Return true if s is a valid string, and false otherwise.
+"""
+
+def isValid(s):
+    #create a map for corresponding values
+    map = {')' : '(', '}' : '{', ']' : '['}
+    #create a stack to keep track of opening characters
+    stack = []
+
+    #iterate through each character in the string
+    for i in s:
+        #if it's not a closing bracket, append it to the stack
+        if i not in map:
+            stack.append(i)
+            continue
+        #if there's nothing in the stack or the characters don't match, return false
+        if not stack or stack[-1] != map[i]:
+            return False
+        #since it did pass, then you can pop the opening bracket off the stack
+        stack.pop()
+    return not stack
+
+
+s1 = "[]"
+s2 = "([{}])"
+s3 = "[(])"
+
+print(isValid(s1))
+print(isValid(s2))
+print(isValid(s3))
